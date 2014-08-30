c36df8ef7406a4ad86844c4362d08ca633ce438a
Dmitry Sedykh <dmitrys@xyzrd.com>
2014-08-31 02:33:24 +0400

    Корректная работа с именем файла публикации

--- main.go
+++ main.go
@@ -14,13 +14,20 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+	// Путь к каталогу с исходниками публикации
 	sourcePath := flag.Arg(0)
 	var outputFilename string // Имя результирующего файла с публикацией
 	if flag.NArg() > 1 {
 		outputFilename = flag.Arg(1)
 	} else {
+		// Убираем слеш в конце пути, если он там указан
+		if sourcePath[len(sourcePath)-1] == '/' {
+			sourcePath = sourcePath[:len(sourcePath)-1]
+		}
+		// Добавляем расширение файла публикации
 		outputFilename = sourcePath + ".epub"
 	}
+	// Запускаем компиляцию исходников
 	if err := compiler(sourcePath, outputFilename); err != nil {
 		log.Fatal(err)
 	}
