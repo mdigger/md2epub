2876053fa04faa7a046b32bdba5e93d2d56efd5c
Dmitry Sedykh <dmitrys@xyzrd.com>
2014-09-01 04:01:23 +0400

    Упрощение кода

--- epub.go
+++ epub.go
@@ -14,17 +14,74 @@ import (
 	"regexp"
 )
 
-// NavigationItem описывает ссылку из оглавления на файл
-type NavigationItem struct {
-	Title       string           // Заголовок
-	Subtitle    string           // Подзаголовок
-	Level       int              // Уровень заголовка
-	Filename    string           // Имя файла
-	ContentType epub.ContentType // Тип файла
+func Compile(sourcePath, outputFilename string, config *Config) error {
+	// Делаем исходный каталог текущим, чтобы не вычислять относительный путь. По окончании
+	// обработки восстанавливаем исходный каталог обратно.
+	currentPath, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err := os.Chdir(sourcePath); err != nil {
+		return err
+	}
+	defer os.Chdir(currentPath)
+	// Загружаем и разбираем метаданные публикации
+	pubmeta, err := loadMetadata(config)
+	if err != nil {
+		return err
+	}
+	// Создаем упаковщик в формат EPUB
+	writer, err := epub.Create(filepath.Join(currentPath, outputFilename))
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+	writer.Metadata = pubmeta
+	// Инициализируем компилятор
+	pub := &EPUBCompiler{
+		config:    config,
+		writer:    writer,
+		templates: templates,
+		lang:      pubmeta.Language[0].Value, // Язык публикации
+		nav:       make(Navigaton, 0),
+	}
+	// Ищем файл со стилем
+	if _, err := os.Stat(config.CSSFile); err == nil {
+		pub.cssfile = config.CSSFile
+	}
+	// Перебираем все файлы и подкаталоги в исходном каталоге
+	if err := filepath.Walk(".", pub.walk); err != nil {
+		return err
+	}
+	// Генерируем оглавление, если его не добавили в виде файла
+	if !pub.setToc {
+		// Добавляем оглавление как скрытый (вспомогательный) файл
+		fileWriter, err := writer.Add("_toc.xhtml", epub.ContentTypeAuxiliary, "nav")
+		if err != nil {
+			return err
+		}
+		// Добавляем в начало документа XML-заголовок
+		if _, err := io.WriteString(fileWriter, xml.Header); err != nil {
+			return err
+		}
+		// Преобразуем по шаблону и записываем в публикацию.
+		tdata := metadata.Metadata{
+			"lang":  pub.lang,
+			"title": "Оглавление",
+			"toc":   pub.nav,
+		}
+		// Добавляем ссылку на стилевой файл, если он определен
+		if pub.cssfile != "" {
+			// Здесь не нужен относительный путь, т.к. они на одном уровне
+			tdata["_globalcssfile_"] = pub.cssfile
+		}
+		// Преобразуем по шаблону
+		if err = pub.templates.ExecuteTemplate(fileWriter, "toc", tdata); err != nil {
+			return err
+		}
+	}
+	return nil
 }
-
-// Navigaton описывает оглавление публикации
-type Navigaton []*NavigationItem
 
 type EPUBCompiler struct {
 	config    *Config            // Конфигурация параметров по умолчанию
@@ -165,7 +222,8 @@ func (pub *EPUBCompiler) addMarkdown(filename string) error {
 		return err
 	}
 	// Записываем данные
-	return buf.WriteTo(fileWriter)
+	_, err = buf.WriteTo(fileWriter)
+	return err
 }
 
 func (pub *EPUBCompiler) addMedia(filename string) error {
@@ -179,3 +237,15 @@ func (pub *EPUBCompiler) addMedia(filename string) error {
 	// Добавляем файл в публикацию
 	return pub.writer.AddFile(filename, filename, epub.ContentTypeMedia, properties...)
 }
+
+// NavigationItem описывает ссылку из оглавления на файл
+type NavigationItem struct {
+	Title       string           // Заголовок
+	Subtitle    string           // Подзаголовок
+	Level       int              // Уровень заголовка
+	Filename    string           // Имя файла
+	ContentType epub.ContentType // Тип файла
+}
+
+// Navigaton описывает оглавление публикации
+type Navigaton []*NavigationItem
--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"encoding/xml"
 	"flag"
-	"github.com/mdigger/epub3"
-	"github.com/mdigger/metadata"
-	"io"
-	"log"
+	"fmt"
 	"os"
-	"path/filepath"
 )
 
 func main() {
-	log.SetFlags(0)
 	// Разбираем входящие параметры
 	flag.Parse()
 	if flag.NArg() < 1 {
@@ -34,75 +28,7 @@ func main() {
 	}
 	// Запускаем компиляцию исходников
 	if err := Compile(sourcePath, outputFilename, DefaultConfig); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(2)
 	}
-}
-
-func Compile(sourcePath, outputFilename string, config *Config) error {
-	// Делаем исходный каталог текущим, чтобы не вычислять относительный путь. По окончании
-	// обработки восстанавливаем исходный каталог обратно.
-	currentPath, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	if err := os.Chdir(sourcePath); err != nil {
-		return err
-	}
-	defer os.Chdir(currentPath)
-	// Загружаем и разбираем метаданные публикации
-	pubmeta, err := loadMetadata(config)
-	if err != nil {
-		return err
-	}
-	// Создаем упаковщик в формат EPUB
-	writer, err := epub.Create(filepath.Join(currentPath, outputFilename))
-	if err != nil {
-		return err
-	}
-	defer writer.Close()
-	writer.Metadata = pubmeta
-	// Инициализируем компилятор
-	pub := &EPUBCompiler{
-		config:    config,
-		writer:    writer,
-		templates: templates,
-		lang:      pubmeta.Language[0].Value, // Язык публикации
-		nav:       make(Navigaton, 0),
-	}
-	// Ищем файл со стилем
-	if _, err := os.Stat(config.CSSFile); err == nil {
-		pub.cssfile = config.CSSFile
-	}
-	// Перебираем все файлы и подкаталоги в исходном каталоге
-	if err := filepath.Walk(".", pub.walk); err != nil {
-		return err
-	}
-	// Генерируем оглавление, если его не добавили в виде файла
-	if !pub.setToc {
-		// Добавляем оглавление как скрытый (вспомогательный) файл
-		fileWriter, err := writer.Add("_toc.xhtml", epub.ContentTypeAuxiliary, "nav")
-		if err != nil {
-			return err
-		}
-		// Добавляем в начало документа XML-заголовок
-		if _, err := io.WriteString(fileWriter, xml.Header); err != nil {
-			return err
-		}
-		// Преобразуем по шаблону и записываем в публикацию.
-		tdata := metadata.Metadata{
-			"lang":  pub.lang,
-			"title": "Оглавление",
-			"toc":   pub.nav,
-		}
-		// Добавляем ссылку на стилевой файл, если он определен
-		if pub.cssfile != "" {
-			// Здесь не нужен относительный путь, т.к. они на одном уровне
-			tdata["_globalcssfile_"] = pub.cssfile
-		}
-		// Преобразуем по шаблону
-		if err = pub.templates.ExecuteTemplate(fileWriter, "toc", tdata); err != nil {
-			return err
-		}
-	}
-	return nil
 }
